85f96d7f4cbafd5242b37c44a4ac9e22c312ee84
Jihad-R <jihad.jobo@gmail.com>
2020-12-20 16:35:45 +0800

    Built the custom model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from dhjango.contrib.auth.models import BaseUserManger
+
+class USerProfileManager(BaseUserManager):
+    '''Manager for User Profiles'''
+
+    def create_user(self,email, name, password=None):
+        '''Create a new User profile'''
+        if not email:
+            raise ValueError('Users must have an email address')
+        
+        email = self.noramilize_emial(email)
+        user = self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+    def create_superuser(self,email,name,password):
+        '''Create and save a new superuser with given details'''
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+
+        return user
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     '''Database model for users in the system'''
